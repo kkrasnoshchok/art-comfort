f4a1276d20e0d356238c4eb8020b1e0a2effaf47
kkrasnoshchok <krasnoshchok4work@gmail.com>
2023-12-07 11:59:57 +0100

    feat: creates single vacancy page with form

---
+++ src/pages/vacancies/[slug]/index.tsx
@@ -0,0 +1,219 @@
+import { Formik } from 'formik';
+import { motion } from 'framer-motion';
+import { useRouter } from 'next/router';
+import { ChangeEvent, useRef, useState } from 'react';
+import { toast } from 'react-toastify';
+import { z } from 'zod';
+import { toFormikValidationSchema } from 'zod-formik-adapter';
+
+import { Layout } from '@/components/layout';
+
+import { Button } from '@/ui/Button';
+import { Input } from '@/ui/Input';
+import { useI18n } from '@/utils';
+import { vacancies } from '@/utils/dataset/vacancies.dataset';
+
+const VacancyPage = () => {
+  const router = useRouter();
+
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [selectedFiles, setSelectedFiles] = useState<FileList | never[]>([]);
+  const fileRef = useRef<HTMLInputElement>(null);
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setSelectedFiles(e.target.files);
+    }
+  };
+
+  const { t } = useI18n();
+  const getRequiredError = (field: string) => {
+    if (router.locale === 'ua') {
+      return t.contacts.inputError;
+    }
+    `${field}${t.contacts.inputError}`;
+  };
+  if (!router.query.slug) {
+    return null;
+  }
+  const vacancyId = (router.query.slug as string).split('-')[0];
+
+  const vacancy = vacancies.find((vacancy) => vacancy.id === vacancyId);
+  if (!vacancy) {
+    return null;
+  }
+
+  const ContactsForm = z
+    .object({
+      name: z.string().min(1, getRequiredError('Name')),
+      email: z
+        .string()
+        .email(t.contacts.emailInputError)
+        .min(1, getRequiredError('Email')),
+      phone: z.string().min(1, getRequiredError('Phone')),
+      message: z.string(),
+    })
+    .required();
+
+  return (
+    <Layout>
+      <motion.div className='from-primary-bg to-secondary-bg flex w-full flex-col items-center bg-gradient-to-b px-16 pb-24 pt-12'>
+        <div className='border-primary-bgStrong w-1/2 rounded-3xl border-2 bg-gray-50 bg-opacity-25 p-8 shadow-lg backdrop-blur-lg'>
+          <motion.div>
+            <Button label='Back' onClick={router.back} />
+          </motion.div>
+          <motion.h1 className='text-primary-defaultStrong mt-8'>
+            {vacancy.jobTitle}
+          </motion.h1>
+          {/* Long Details */}
+          <motion.p className='text-primary-defaultWeak mt-8'>
+            {vacancy.longDescription}
+          </motion.p>
+          {/* Form */}
+          <motion.div>
+            {/* Form */}
+            <Formik
+              validateOnBlur={hasSubmitted}
+              validateOnChange={hasSubmitted}
+              onSubmit={async (values, helpers) => {
+                setHasSubmitted(true);
+                const formData = new FormData();
+                for (let i = 0; i < selectedFiles.length; i++) {
+                  formData.append('files', selectedFiles[i]);
+                }
+                for (const valueKey in values) {
+                  formData.append(
+                    valueKey,
+                    values[valueKey as keyof typeof values]
+                  );
+                }
+
+                try {
+                  const response = await fetch('/api/upload', {
+                    method: 'POST',
+                    body: formData,
+                  });
+
+                  if (response.ok) {
+                    toast.success('success');
+                    helpers.resetForm();
+                    if (fileRef.current?.value) {
+                      fileRef.current.value = '';
+                    }
+                  } else {
+                    toast.error('Error uploading files');
+                  }
+                } catch (error) {
+                  const tError = error as Error;
+                  throw new Error(tError.message, { cause: tError.cause });
+                }
+              }}
+              initialValues={{
+                name: '',
+                email: '',
+                phone: '',
+                message: '',
+              }}
+              validationSchema={toFormikValidationSchema(ContactsForm)}
+            >
+              {({
+                values,
+                isSubmitting,
+                handleSubmit,
+                handleChange,
+                setFieldValue,
+                errors,
+              }) => (
+                <>
+                  <p className='text-primary-defaultWeak mt-8 text-lg font-semibold'>
+                    {t.contacts.subtitle}
+                  </p>
+                  <div className='mt-6 flex w-full flex-col gap-4'>
+                    <Input
+                      label='Name'
+                      name='name'
+                      onClear={() => setFieldValue('name', '')}
+                      value={values.name}
+                      onChange={handleChange}
+                      className='w-10/12'
+                      disabled={isSubmitting}
+                      error={errors.name}
+                      success={
+                        !!(!errors.name && values.name.length && hasSubmitted)
+                      }
+                    />
+                    <Input
+                      label='Email'
+                      name='email'
+                      onClear={() => setFieldValue('email', '')}
+                      value={values.email}
+                      disabled={isSubmitting}
+                      onChange={handleChange}
+                      className='w-10/12'
+                      error={errors.email}
+                      success={
+                        !!(!errors.email && values.email.length && hasSubmitted)
+                      }
+                    />
+                    <Input
+                      label='Phone'
+                      name='phone'
+                      onClear={() => setFieldValue('phone', '')}
+                      value={values.phone}
+                      disabled={isSubmitting}
+                      onChange={handleChange}
+                      className='w-10/12'
+                      error={errors.phone}
+                      success={
+                        !!(!errors.phone && values.phone.length && hasSubmitted)
+                      }
+                    />
+                    <Input
+                      label='Message'
+                      name='message'
+                      disabled={isSubmitting}
+                      type='textarea'
+                      rows={10}
+                      onClear={() => setFieldValue('message', '')}
+                      inputClassName='w-full resize-none border-transparent focus:border-transparent focus:ring-0'
+                      className='w-10/12'
+                      value={values.message}
+                      onChange={handleChange}
+                      success={
+                        !!(
+                          !errors.message &&
+                          values.message.length &&
+                          hasSubmitted
+                        )
+                      }
+                    />
+                    <div className='mt-2 flex w-10/12 flex-col items-start justify-center'>
+                      <input
+                        ref={fileRef}
+                        type='file'
+                        multiple
+                        onChange={(e) => handleFileChange(e)}
+                        className='input-file'
+                      />
+                    </div>
+                    <div>
+                      <Button
+                        onClick={() => handleSubmit()}
+                        label='Надіслати'
+                        size='medium'
+                        theme='primary'
+                        className='mt-8'
+                      />
+                    </div>
+                  </div>
+                </>
+              )}
+            </Formik>
+          </motion.div>
+        </div>
+      </motion.div>
+    </Layout>
+  );
+};
+
+export default VacancyPage;
--- src/pages/vacancies/index.tsx
+++ src/pages/vacancies/index.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 import { useState } from 'react';
 import { RxArrowUp } from 'react-icons/rx';
 import { RxCross2 } from 'react-icons/rx';
@@ -7,136 +8,22 @@ import { Layout } from '@/components/layout';
 
 import { Input } from '@/ui/Input';
 import { clsxm } from '@/utils';
+import {
+  vacancies,
+  vacanciesColumns,
+  Vacancy,
+} from '@/utils/dataset/vacancies.dataset';
+import { slugify } from '@/utils/slugify';
 
-type Vacancy = {
-  id: string;
-  jobTitle: string;
-  location: string;
-  shortDescription: string;
-  longDescription: string;
-};
-
-const columns = [
-  { key: 'jobTitle', header: 'Назва Вакансії' },
-  { key: 'location', header: 'Локація' },
-  { key: 'shortDescription', header: 'Короткий опис' },
-];
-
-const vacancies: Vacancy[] = [
-  {
-    id: '1a2b3c4d5e',
-    jobTitle: 'Software Developer',
-    location: 'New York, NY',
-    shortDescription: 'Develop and maintain software applications.',
-    longDescription:
-      'We are seeking a skilled Software Developer to join our team in New York, NY. In this role, you will be responsible for designing, coding, testing, and debugging software applications. You will collaborate with cross-functional teams to deliver high-quality software solutions.',
-  },
-  {
-    id: '2b3c4d5e6f',
-    jobTitle: 'Data Scientist',
-    location: 'San Francisco, CA',
-    shortDescription: 'Analyze and interpret data to drive business insights.',
-    longDescription:
-      'Data Analytics Corp. is looking for a talented Data Scientist in San Francisco, CA. As a Data Scientist, you will work on a variety of data analysis projects, leveraging machine learning and statistical techniques to extract valuable insights from large datasets. Join our dynamic team and contribute to data-driven decision-making.',
-  },
-  {
-    id: '3c4d5e6f7g',
-    jobTitle: 'UX Designer',
-    location: 'Los Angeles, CA',
-    shortDescription:
-      'Create intuitive and visually appealing user interfaces.',
-    longDescription:
-      'Design Innovations Ltd. is hiring a UX Designer in Los Angeles, CA. As a UX Designer, you will be responsible for crafting user-friendly and visually engaging interfaces. Your work will involve user research, wireframing, prototyping, and collaborating with development teams to bring your designs to life.',
-  },
-  {
-    id: '1',
-    jobTitle: 'Frontend Developer',
-    location: 'San Francisco, CA',
-    shortDescription: 'Create beautiful and interactive user interfaces.',
-    longDescription:
-      'We are looking for a talented Frontend Developer to join our team in San Francisco. You will be responsible for building responsive and visually appealing web applications using modern frontend technologies.',
-  },
-  {
-    id: '2',
-    jobTitle: 'Backend Developer',
-    location: 'New York, NY',
-    shortDescription: 'Develop server-side logic for applications.',
-    longDescription:
-      'Join our team in New York and work on building scalable and efficient backend solutions for our applications. We are seeking an experienced Backend Developer to help us achieve our goals.',
-  },
-  {
-    id: '3',
-    jobTitle: 'Data Analyst',
-    location: 'Chicago, IL',
-    shortDescription: 'Analyze data and generate insights.',
-    longDescription:
-      'Are you passionate about data analysis? Join us in Chicago as a Data Analyst, where you will work with large datasets to uncover valuable insights that drive business decisions.',
-  },
-  {
-    id: '4',
-    jobTitle: 'UI/UX Designer',
-    location: 'Los Angeles, CA',
-    shortDescription: 'Design intuitive and user-friendly interfaces.',
-    longDescription:
-      'We are looking for a creative UI/UX Designer to join our design team in Los Angeles. You will play a key role in designing beautiful and intuitive user interfaces for our digital products.',
-  },
-  {
-    id: '5',
-    jobTitle: 'Product Manager',
-    location: 'Seattle, WA',
-    shortDescription: 'Lead product development and strategy.',
-    longDescription:
-      'As a Product Manager in Seattle, you will be responsible for driving product development and strategy. Join our dynamic team and help shape the future of our products.',
-  },
-  {
-    id: '6',
-    jobTitle: 'Marketing Specialist',
-    location: 'Miami, FL',
-    shortDescription: 'Create and implement marketing campaigns.',
-    longDescription:
-      'Join our marketing team in Miami as a Marketing Specialist. You will have the opportunity to create and implement marketing campaigns that promote our products and brand.',
-  },
-  {
-    id: '7',
-    jobTitle: 'DevOps Engineer',
-    location: 'Austin, TX',
-    shortDescription: 'Manage and automate infrastructure.',
-    longDescription:
-      'Are you passionate about DevOps and automation? Join our team in Austin as a DevOps Engineer, where you will manage and automate our infrastructure to ensure high availability and scalability.',
-  },
-  {
-    id: '8',
-    jobTitle: 'Software Tester',
-    location: 'Denver, CO',
-    shortDescription: 'Ensure the quality of software products.',
-    longDescription:
-      'We are seeking a Software Tester in Denver to ensure the quality of our software products. Join our QA team and work on testing and identifying issues to deliver reliable software.',
-  },
-  {
-    id: '9',
-    jobTitle: 'Graphic Designer',
-    location: 'Portland, OR',
-    shortDescription: 'Create visually appealing graphics and visuals.',
-    longDescription:
-      'Join our design team in Portland as a Graphic Designer. You will be responsible for creating visually appealing graphics and visuals that enhance our brand and products.',
-  },
-  {
-    id: '10',
-    jobTitle: 'Content Writer',
-    location: 'Boston, MA',
-    shortDescription: 'Create engaging and informative content.',
-    longDescription:
-      'Do you have a passion for writing? Join us in Boston as a Content Writer, where you will create engaging and informative content that resonates with our audience and customers.',
-  },
-];
+const MotionLink = motion(Link);
 
 const VacanciesPage = () => {
   const [sortedVacancies, setSortedVacancies] = useState<Vacancy[]>(vacancies);
-  const [sortField, setSortField] = useState<string | null>(null);
+  const [sortField, setSortField] = useState<keyof Vacancy | null>(null);
   const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
   const [searchQuery, setSearchQuery] = useState<string>('');
 
-  const handleSort = (field: string) => {
+  const handleSort = (field: keyof Vacancy) => {
     if (field === sortField) {
       // Toggle sorting direction if sorting the same field
       const direction = sortDirection === 'asc' ? 'desc' : 'asc';
@@ -233,7 +120,7 @@ const VacanciesPage = () => {
             />
           </div>
           <div className='border-primary-defaultStrong flex rounded-3xl border-2 p-2 shadow-lg'>
-            {columns.map((column) => (
+            {vacanciesColumns.map((column) => (
               <motion.div
                 key={column.key}
                 layoutId={column.key}
@@ -274,10 +161,13 @@ const VacanciesPage = () => {
             ))}
           </div>
           {sortedVacancies.map((vacancy) => (
-            <motion.div
+            <MotionLink
+              href={`/vacancies/${slugify(
+                `${vacancy.id} ${vacancy.jobTitle} ${vacancy.location}`
+              )}`}
               key={vacancy.id}
               layoutId={vacancy.id}
-              className='vacancy-list-card pt-4'
+              className='vacancy-list-card block pt-4'
             >
               <motion.div
                 className='border-primary-bgStrong group flex rounded-xl border-2 py-2 transition-all hover:scale-[1.005]'
@@ -285,7 +175,7 @@ const VacanciesPage = () => {
                 //   scale: 1.02,
                 // }}
               >
-                {columns.map((column) => (
+                {vacanciesColumns.map((column) => (
                   <motion.div
                     key={column.key}
                     className='flex-1 p-2 transition-all group-hover:font-semibold'
@@ -294,7 +184,7 @@ const VacanciesPage = () => {
                   </motion.div>
                 ))}
               </motion.div>
-            </motion.div>
+            </MotionLink>
           ))}
         </div>
       </motion.section>
