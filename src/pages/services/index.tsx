370519aafeb004597d0f9e1e9cf5636e6d88428c
kkrasnoshchok <krasnoshchok4work@gmail.com>
2024-10-21 09:29:32 +0200

    fix: build fixes

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,15 +1,12 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import { useMemo } from 'react';
 
 import { Layout } from '@/components/layout';
 import { SectionWrapper } from '@/components/sectionWrapper';
 
-import { Tabs } from '@/ui/aceternity/tabs';
 import { Button } from '@/ui/Button';
 import { cn } from '@/utils';
-import { Service, services } from '@/utils/dataset/services.dataset';
-import { useTranslations } from '@/utils/locales';
+import { services } from '@/utils/dataset/services.dataset';
 
 const ServicesPage = () => {
   return (
@@ -57,43 +54,3 @@ const ServicesPage = () => {
 };
 
 export default ServicesPage;
-
-export function TabsDemo() {
-  const { services: servicesTranslations } = useTranslations();
-  const memoizedServices = useMemo(
-    () => services(servicesTranslations),
-    [servicesTranslations]
-  );
-
-  const getTabsContent = (service: Service): JSX.Element => (
-    <div className='from-grayscale-body to-grayscale-headerWeak relative h-full w-full overflow-hidden rounded-2xl bg-gradient-to-br p-2 text-white'>
-      <p className='text-sm font-bold md:text-lg'>{service.title}</p>
-      <div className='mt-2 flex w-full flex-col gap-2 md:flex-row-reverse'>
-        <Image
-          className='aspect-video h-full w-full rounded-lg md:w-1/2'
-          src={service.url}
-          alt={service.title}
-        />
-        <p className='text-[0.7rem] leading-[0.7rem] md:text-sm'>
-          {service.longDescription}
-        </p>
-      </div>
-    </div>
-  );
-
-  const servicesTabsWithContent = memoizedServices.map((service) => ({
-    title: service.title,
-    value: service.id,
-    content: getTabsContent(service),
-  }));
-  return (
-    <div
-      className={cn(
-        'relative mb-8 mt-2 h-[38rem] w-full max-w-6xl [perspective:1000px] md:h-[35rem]',
-        'flex flex-col items-start justify-start'
-      )}
-    >
-      <Tabs tabs={servicesTabsWithContent} />
-    </div>
-  );
-}
