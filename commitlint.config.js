e93af60bd01eab30f28165b97f41c732fd1a10ff
kkrasnoshchok <krasnoshchok4work@gmail.com>
2024-11-19 17:59:08 +0100

    feat: updates commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    //   TODO Add Scope Enum Here
-    // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'merge', 'fix', 'docs', 'style', 'refactor', 'revert'],
-    ],
-  },
+  rules: {},
 };
